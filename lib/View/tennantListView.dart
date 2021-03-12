75c73bae6f8e9cd670c1d55cf6743326f9d69177
Daniel Iversen <daiverse@stud.ntnu.no>
2021-03-12 11:18:37 +0100

    Added view for log and tennats
    Can list the tennants and the log. NOT WORKING WITH API

--- lib/View/tennantListView.dart
+++ lib/View/tennantListView.dart
@@ -11,17 +11,42 @@ class Tennant_View extends StatefulWidget {
 
 class _Tennant_ViewState extends State<Tennant_View> {
 
-  List data;
+  List data = [
+    {
+      "city": "Hamresanden",
+      "address": "Juraveien 4",
+      "zipcode": "4656",
+      "tennant_id": "1",
+      "business_id": "123456789",
+      "tennant_name": "ElProffen"
+    },
+    {
+      "city": "Laksevåg",
+      "address": "Håsteins gate 10",
+      "zipcode": "5160",
+      "tennant_id": "2",
+      "business_id": "123789456",
+      "tennant_name": "Kamo Ent. AS"
+    },
+    {
+      "city": "Ulsteinvik",
+      "address": "Smårisevadet 19",
+      "zipcode": "6065",
+      "tennant_id": "4",
+      "business_id": "321654897",
+      "tennant_name": "Kuldepartner AS"
+    }
+  ];
 
   @override
   Widget build(BuildContext context) {
 
-    data = ModalRoute.of(context).settings.arguments;
+    print(data);
 
     return new Scaffold(
       appBar: new AppBar(
         backgroundColor: Colors.orange[800],
-        title: Text("Hente info"),
+        title: Text("Liste over tennants"),
       ),
       body: Container(
         decoration: BoxDecoration(
@@ -44,7 +69,7 @@ class _Tennant_ViewState extends State<Tennant_View> {
                             mainAxisAlignment: MainAxisAlignment.center,
                             children: [
                               new Text(
-                                "",
+                                "Navnet på bedriften",
                                 style: TextStyle(
                                   fontSize: 24,
                                   fontWeight: FontWeight.bold,
@@ -52,23 +77,30 @@ class _Tennant_ViewState extends State<Tennant_View> {
                               ),
                               SizedBox(height: 6),
                               new Text(
-                                "",
+                                "Orgnummeret",
                                 style: TextStyle(
                                     fontSize: 20
                                 ),
                               ),
                               SizedBox(height: 6),
                               new Text(
-                                "",
+                                "City",
                                 style: TextStyle(
                                     fontSize: 20
                                 ),
                               ),
                               SizedBox(height: 6),
                               new Text(
-                                "",
+                                "Address",
                                 style: TextStyle(
                                     fontSize: 20
+                                ),
+                              ),
+                              SizedBox(height: 6),
+                              new Text(
+                                "Zipcode",
+                                style: TextStyle(
+                                  fontSize: 20,
                                 ),
                               ),
                             ],
